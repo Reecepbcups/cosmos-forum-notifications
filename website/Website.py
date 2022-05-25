1ac22ec5fde0caf9b7d11b12a41926389ee3bb18
Reece Williams <Reecepbcups@gmail.com>
2022-05-24 23:48:51 -0500

    Website w/ working checkboxes

--- website/Website.py
+++ website/Website.py
@@ -1,19 +1,33 @@
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
 
+# https://testdriven.io/blog/flask-sessions/
+app.secret_key = 'BAD_SECRET_KEY'
 
 @app.route('/')
 def my_form():
-    return render_template('my-form.html')
+    return render_template('login-form.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['webhookURL']
-    processed_text = text.upper()
-    print(processed_text)
-    return processed_text
+    # processed_text = text.upper()
+    # print(processed_text)
+    # session['webhook'] = text
+    # return processed_text
+    return redirect(url_for('main_page'))
+    
+@app.route("/test" , methods=['GET', 'POST'])
+def test():
+    select = request.form.getlist('chains')
+    return(str(select)) # just to see what select is
+
+@app.route('/main')
+def main_page():
+    # myWebhook = session.get('webhook', None)
+    return render_template('main-page.html',  data=[{'chain': 'Juno'}, {'chain': 'Osmosis'}])
 
 # run the app
 if __name__ == "__main__":
